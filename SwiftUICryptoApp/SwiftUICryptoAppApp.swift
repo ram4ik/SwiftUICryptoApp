90794e0aaf86152b756a5d603789c25864594c71
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-25 20:58:58 +0300

    Initial Commit

---
+++ SwiftUICryptoApp/SwiftUICryptoAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUICryptoAppApp.swift
+//  SwiftUICryptoApp
+//
+//  Created by Ramill Ibragimov on 25.05.2021.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUICryptoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
