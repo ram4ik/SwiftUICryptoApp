4a6ae23831ab9b838719c12de573b293b62709e2
Ramill Ibragimov <Ramill_Ibragimov@icloud.com>
2021-05-26 20:42:37 +0300

    HomeView

---
+++ SwiftUICryptoApp/Core/Home/Views/HomeView.swift
@@ -0,0 +1,60 @@
+//
+//  HomeView.swift
+//  SwiftUICryptoApp
+//
+//  Created by Ramill Ibragimov on 26.05.2021.
+//
+
+import SwiftUI
+
+struct HomeView: View {
+    @State private var showPortfolio: Bool = false
+    
+    var body: some View {
+        ZStack {
+            Color.theme.background
+                .ignoresSafeArea()
+            
+            VStack {
+                homeheader
+                Spacer(minLength: 0)
+            }
+        }
+    }
+}
+
+struct HomeView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView {
+            HomeView()
+                .navigationBarHidden(true)
+        }
+    }
+}
+
+extension HomeView {
+    private var homeheader: some View {
+        HStack {
+            CircleButtonView(iconName: showPortfolio ? "plus" : "info")
+                .animation(.none)
+                .background(
+                    CircleButtonAnimationView(animate: $showPortfolio)
+                )
+            Spacer()
+            Text(showPortfolio ? "Portfolio" : "Live Prices")
+                .font(.headline)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.theme.accent)
+                .animation(.none)
+            Spacer()
+            CircleButtonView(iconName: "chevron.right")
+                .rotationEffect(Angle(degrees: showPortfolio ? 180 : 0))
+                .onTapGesture {
+                    withAnimation(.spring()) {
+                        showPortfolio.toggle()
+                    }
+                }
+        }
+        .padding(.horizontal)
+    }
+}
--- SwiftUICryptoApp/SwiftUICryptoAppApp.swift
+++ SwiftUICryptoApp/SwiftUICryptoAppApp.swift
@@ -11,7 +11,10 @@ import SwiftUI
 struct SwiftUICryptoAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            NavigationView {
+                HomeView()
+                    .navigationBarHidden(true)
+            }
         }
     }
 }
