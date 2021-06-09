cee6e0e947d58f41f6e9714ed0a7d1508f6b002c
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-09 11:20:32 +0300

    Saving current user's portfolio to Core Data with MVVM structure

--- SwiftUICryptoApp/Core/Home/Views/PortfolioView.swift
+++ SwiftUICryptoApp/Core/Home/Views/PortfolioView.swift
@@ -55,13 +55,13 @@ extension PortfolioView {
     private var coinLogoList: some View {
         ScrollView(.horizontal, showsIndicators: false, content: {
             LazyHStack(spacing: 10) {
-                ForEach(vm.allCoins) { coin in
+                ForEach(vm.searchText.isEmpty ? vm.portfolioCoins : vm.allCoins) { coin in
                     CoinLogoView(coin: coin)
                         .frame(width: 75)
                         .padding(4)
                         .onTapGesture {
                             withAnimation(.easeIn) {
-                                selectedCoin = coin
+                                updateSelectedCoin(coin: coin)
                             }
                         }
                         .background(RoundedRectangle(cornerRadius: 10)
@@ -71,6 +71,16 @@ extension PortfolioView {
             .frame(height: 120)
             .padding(.leading)
         })
+    }
+    
+    private func updateSelectedCoin(coin: CoinModel) {
+        selectedCoin = coin
+        if let portfolioCoin = vm.portfolioCoins.first(where: { $0.id == coin.id }),
+           let amount = portfolioCoin.currentHoldings {
+            quantityText = "\(amount)"
+        } else {
+            quantityText = ""
+        }
     }
     
     private func getCurrentValue() -> Double {
@@ -124,10 +134,12 @@ extension PortfolioView {
     }
     
     private func saveButtonPressed() {
-        guard let coin = selectedCoin else {
-            return
-        }
-        // save to portfolio
+        guard
+            let coin = selectedCoin,
+            let amount = Double(quantityText)
+            else { return }
+        
+        vm.updatePortfolio(coin: coin, amount: amount)
         
         withAnimation(.easeIn) {
             showCheckMark = true
