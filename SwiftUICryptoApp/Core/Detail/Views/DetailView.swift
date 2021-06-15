2bff685c901251214a302b1f69c8d02dcf60c7ec
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-15 09:26:00 +0300

    Creating a custom line chart to display price data with animation and shadow

--- SwiftUICryptoApp/Core/Detail/Views/DetailView.swift
+++ SwiftUICryptoApp/Core/Detail/Views/DetailView.swift
@@ -33,19 +33,27 @@ struct DetailView: View {
     
     var body: some View {
         ScrollView {
-            VStack(spacing: 20) {
-                Text("")
-                    .frame(height: 150)
-                overviewTitle
-                Divider()
-                overviewGrid
-                additionalTitle
-                Divider()
-                additionalGrid
+            VStack {
+                ChartView(coin: vm.coin)
+                    .padding(.vertical)
+                
+                VStack(spacing: 20) {
+                    overviewTitle
+                    Divider()
+                    overviewGrid
+                    additionalTitle
+                    Divider()
+                    additionalGrid
+                }
+                .padding()
             }
-            .padding()
         }
         .navigationTitle(vm.coin.name)
+        .toolbar {
+            ToolbarItem(placement: .navigationBarTrailing) {
+                navigationBarTrailingItems
+            }
+        }
     }
 }
 
@@ -58,6 +66,16 @@ struct DetailView_Previews: PreviewProvider {
 }
 
 extension DetailView {
+    private var navigationBarTrailingItems: some View {
+        HStack {
+            Text(vm.coin.symbol.uppercased())
+                .font(.headline)
+                .foregroundColor(Color.theme.secondaryText)
+            CoinImageView(coin: vm.coin)
+                .frame(width: 25, height: 25)
+        }
+    }
+    
     private var overviewTitle: some View {
         Text("Overview")
             .font(.title)
---
+++ SwiftUICryptoApp/Extensions/Date.swift
@@ -0,0 +1,29 @@
+//
+//  Date.swift
+//  SwiftUICryptoApp
+//
+//  Created by ramil on 15.06.2021.
+//
+
+import Foundation
+
+extension Date {
+    
+    // "2021-03-13T20:49:26.606Z"
+    init(coinGeckoString: String) {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd'T'HH:mm:ss.SSSZ"
+        let date = formatter.date(from: coinGeckoString) ?? Date()
+        self.init(timeInterval: 0, since: date)
+    }
+    
+    private var shortFormatter: DateFormatter {
+        let formatter = DateFormatter()
+        formatter.dateStyle = .short
+        return formatter
+    }
+    
+    func asShortDateString() -> String {
+        return shortFormatter.string(from: self)
+    }
+}
