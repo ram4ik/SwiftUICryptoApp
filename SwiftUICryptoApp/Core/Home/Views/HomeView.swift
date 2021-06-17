3d07ceb79248627add919dfe83c83865fb85b2e9
Ramill Ibragimov <ramill_ibragimov@icloud.com>
2021-06-17 11:04:17 +0300

    Adding a Settings screen using a List with Sections

--- SwiftUICryptoApp/Core/Home/Views/HomeView.swift
+++ SwiftUICryptoApp/Core/Home/Views/HomeView.swift
@@ -11,7 +11,7 @@ struct HomeView: View {
     @EnvironmentObject private var vm: HomeViewModel
     @State private var showPortfolio: Bool = false
     @State private var showPortfolioView: Bool = false
-    
+    @State private var showSettingsView: Bool = false
     @State private var selectedCoin: CoinModel? = nil
     @State private var showDetailView: Bool = false
     
@@ -40,6 +40,9 @@ struct HomeView: View {
                 }
                 Spacer(minLength: 0)
             }
+            .sheet(isPresented: $showSettingsView, content: {
+                SettingsView()
+            })
         }
         .background(
             NavigationLink(
@@ -67,6 +70,8 @@ extension HomeView {
                 .onTapGesture {
                     if showPortfolio {
                         showPortfolioView.toggle()
+                    } else {
+                        showSettingsView.toggle()
                     }
                 }
                 .background(
---
+++ SwiftUICryptoApp/Core/Settings/Views/SettingsView.swift
@@ -0,0 +1,99 @@
+//
+//  SettingsView.swift
+//  SwiftUICryptoApp
+//
+//  Created by ramil on 17.06.2021.
+//
+
+import SwiftUI
+
+struct SettingsView: View {
+    let defaultURL = URL(string: "https://www.google.com")!
+    let youtubeURL = URL(string: "https://www.youtube.com/c/swiftfulthinking")!
+    let coffeeURL = URL(string: "https://www.buymeacoffee.com/nicksarno")!
+    let coingeckoURL = URL(string: "https://www.coingecko.com")!
+    let personalURL = URL(string: "https://github.com/ram4ik/SwiftUICryptoApp")!
+    
+    var body: some View {
+        NavigationView {
+            List {
+                swiftfulSection
+                coinGeckoSection
+                developerSection
+            }
+            .font(.headline)
+            .accentColor(.blue)
+            .listStyle(GroupedListStyle())
+            .navigationTitle("Settings")
+            .toolbar(content: {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    XMarkButton()
+                }
+            })
+        }
+    }
+}
+
+struct SettingsView_Previews: PreviewProvider {
+    static var previews: some View {
+        SettingsView()
+    }
+}
+
+extension SettingsView {
+    
+    private var swiftfulSection: some View {
+        Section(header: Text("Swiftful Thinking")) {
+            VStack(alignment: .leading) {
+                Image("logo")
+                    .resizable()
+                    .frame(width: 100, height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was made by following a @SwiftfulThinking course on YouTube. It uses MVVM Architecture, Combine, and CoreData!")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Subscribe on YouTube 🥳", destination: youtubeURL)
+            Link("Support his coffee addiction ☕️", destination: coffeeURL)
+        }
+    }
+    
+    private var coinGeckoSection: some View {
+        Section(header: Text("CoinGecko")) {
+            VStack(alignment: .leading) {
+                Image("coingecko")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("The cryptocurrency data that is used in thos app comes from a free API from CoinGecko! Prices may be slightly delayed.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit CoinGecko 🦎", destination: coingeckoURL)
+        }
+    }
+    
+    private var developerSection: some View {
+        Section(header: Text("Developer")) {
+            VStack(alignment: .leading) {
+                Image("19762704")
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 100)
+                    .clipShape(RoundedRectangle(cornerRadius: 20))
+                Text("This app was developer by Nick Sarno. It uses SwiftUI and its written 100% in Swift. The project benefits from multi-threading, publishers/subscribers, and data persistance.")
+                    .font(.callout)
+                    .fontWeight(.medium)
+                    .foregroundColor(Color.theme.accent)
+            }
+            .padding(.vertical)
+            Link("Visit Website 😎", destination: personalURL)
+        }
+    }
+    
+}
